c90a9649ff333054cb493664b2b8c6f3b6674784
duongnt34 <duongntdev@outlook.com.vn>
2023-08-22 17:13:29 +0700

    second

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./node_modules/flowbite/**/*.{js,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        outline: "0 4px 12px rgba(0,0,0,.04)",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
